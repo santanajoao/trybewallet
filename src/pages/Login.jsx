3942351fd27402a79c6b7c7f7d72e7bf885f17b4
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-01 15:28:50 -0300

    estrutura jsx básica da página de login criada

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="Login">
+        <form className="Login__form">
+          <input type="email" name="" id="" />
+          <input type="password" name="" id="" />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
