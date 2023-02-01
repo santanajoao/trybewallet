e20f49dadd2273d5c7d6123318db860be1e57d12
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-01 16:15:26 -0300

    funcionalidades de Login que não envolvem redux criadas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  areFieldsInvalid = () => {
+    const { email, password } = this.state;
+    const passwordMinLength = 6;
+    const invalidEmail = !/[\w\d]+@[\w\d]+\.com/.test(email);
+    const invalidPassword = password.length < passwordMinLength;
+    return invalidEmail || invalidPassword;
+  };
+
   render() {
+    const { email, password } = this.state;
     return (
       <div className="Login">
-        <form className="Login__form">
-          <input type="email" name="" id="" />
-          <input type="password" name="" id="" />
-          <button type="submit">Entrar</button>
+        <form onSubmit={ this.handleSubmit } className="Login__form">
+          <input
+            value={ email }
+            type="email"
+            placeholder="E-mail"
+            onChange={ this.handleChange }
+            name="email"
+            data-testid="email-input"
+          />
+          <input
+            value={ password }
+            type="password"
+            placeholder="Senha"
+            onChange={ this.handleChange }
+            name="password"
+            data-testid="password-input"
+          />
+          <button type="submit" disabled={ this.areFieldsInvalid() }>
+            Entrar
+          </button>
         </form>
       </div>
     );
