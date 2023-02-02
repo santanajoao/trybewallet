a5b8662b6ed23467d9c559c4fe256319fa990498
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-02 09:46:10 -0300

    todas as chaves da store adicionadas

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -2,6 +2,9 @@ import { SET_CURRENCIES } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
 };
 
 const walletReducer = (state = INITIAL_STATE, action) => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,10 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, composeWithDevTools(
-  applyMiddleware(thunk),
-));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 if (window.Cypress) {
   window.store = store;
