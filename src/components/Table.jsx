278cd98bb544b72fb50265959845555ad770d0c2
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-02 16:03:45 -0300

    componente TableRow criado e requisito 07 resolvido

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import TableRow from './TableRow';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table className="Table">
         <thead>
@@ -18,10 +21,21 @@ class Table extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          { expenses.map((expense) => (
+            <TableRow key={ expense.id } expense={ expense } />
+          )) }
+        </tbody>
       </table>
     );
   }
 }
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.object.isRequired,
+  ).isRequired,
+};
 
 const mapStateToProps = ({ wallet }) => ({
   expenses: wallet.expenses,
---
+++ src/components/TableRow.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class TableRow extends Component {
+  formatNumericValue = (value) => {
+    const numberValue = Number(value);
+    return numberValue.toFixed(2);
+  };
+
+  render() {
+    const { expense } = this.props;
+    const {
+      exchangeRates, currency, value, description, tag, method,
+    } = expense;
+
+    const { ask, name } = exchangeRates[currency];
+    const convertedValue = value * ask;
+    return (
+      <tr>
+        <td>{ description }</td>
+        <td>{ tag }</td>
+        <td>{ method }</td>
+        <td>{ this.formatNumericValue(value) }</td>
+        <td>{ name }</td>
+        <td>{ this.formatNumericValue(ask) }</td>
+        <td>{ this.formatNumericValue(convertedValue) }</td>
+        <td>Real</td>
+      </tr>
+    );
+  }
+}
+
+TableRow.propTypes = {
+  expense: PropTypes.shape({
+    exchangeRates: PropTypes.objectOf(
+      PropTypes.shape({
+        ask: PropTypes.string,
+        name: PropTypes.string,
+      }),
+    ),
+    currency: PropTypes.string,
+    value: PropTypes.string,
+    description: PropTypes.string,
+    tag: PropTypes.string,
+    method: PropTypes.string,
+  }).isRequired,
+};
