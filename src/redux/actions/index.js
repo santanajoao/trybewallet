d3991ef147ac1f1a69a1515002964c73f3371cb9
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-02 09:50:49 -0300

    constante ADD_EXPENSE adicionada

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,6 @@
 export const LOGIN = 'LOGIN';
 export const SET_CURRENCIES = 'SET_CURRENCIES';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
 
 export const login = (email) => ({
   type: LOGIN,
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { SET_CURRENCIES } from '../actions';
+import { SET_CURRENCIES, ADD_EXPENSE } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
