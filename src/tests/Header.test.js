60334ebe1e4c866df23b31698b06ea5dc86a492c
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-02 15:07:10 -0300

    testes do Header implementados

---
+++ src/tests/Header.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+import App from '../App';
+import mockData from './helpers/mockData';
+
+describe('Header', () => {
+  const validEmail = 'joao123@gmail.com';
+  it('o email digitado ao logar aparece no header', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    userEvent.type(screen.getByRole('textbox'), validEmail);
+    userEvent.type(screen.getByPlaceholderText('Senha'), '123456');
+    userEvent.click(screen.getByRole('button', { name: 'Entrar' }));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/carteira');
+
+    screen.getByText(validEmail);
+  });
+
+  it('o total inicia com 0', () => {
+    renderWithRouterAndRedux(<App />);
+    userEvent.type(screen.getByRole('textbox'), validEmail);
+    userEvent.type(screen.getByPlaceholderText('Senha'), '123456');
+    userEvent.click(screen.getByRole('button', { name: 'Entrar' }));
+    expect(screen.getByTestId('total-field')).toHaveTextContent('0.00');
+  });
+
+  it('ao adicionar uma despesa o total é atualizado', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockData),
+    });
+
+    renderWithRouterAndRedux(<App />);
+    userEvent.type(screen.getByRole('textbox'), validEmail);
+    userEvent.type(screen.getByPlaceholderText('Senha'), '123456');
+    userEvent.click(screen.getByRole('button', { name: 'Entrar' }));
+
+    userEvent.type(screen.getByRole('spinbutton', { name: 'Valor' }), '1');
+
+    act(() => {
+      userEvent.click(screen.getByRole('button', { name: 'Adicionar despesa' }));
+    });
+
+    await screen.findByText('4.75');
+  });
+});
